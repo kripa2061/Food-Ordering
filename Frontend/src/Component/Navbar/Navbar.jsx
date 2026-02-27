594dea8ac74cbdab2adc2293051a045391aa7c17
KripaBastola <kripabastola73@gmail.com>
2026-02-27 10:37:13 +0545

    responsive UI

--- Frontend/src/Component/Navbar/Navbar.jsx
+++ Frontend/src/Component/Navbar/Navbar.jsx
@@ -1,19 +1,16 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { assets } from '../../assets/frontend_assets/assets';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
-import { StoreContext } from "../../Context/Context";
-
+import { StoreContext } from '../../Context/Context';
+import axios from 'axios';
 const Navbar = ({ setShowLogin }) => {
-  const { cartItem, totalItems, token, setToken, userData, setUserData } = useContext(StoreContext);
-
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    localStorage.removeItem("userData");
+  const [menu, setMenu] = useState("home");
+  const { cartItem, totalItems, token, setToken, url, userData } = useContext(StoreContext);
+  const handleLogout = async () => {
+    localStorage.removeItem("token")
     setToken("");
-    setUserData(null);
-  };
-console.log("Navbar userData:", userData);
+  }
   return (
     <div className="container">
       <div className="logo">
@@ -23,34 +20,71 @@ console.log("Navbar userData:", userData);
 
       <div className="nav">
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><a href="#explore-menu">Menu</a></li>
-          <li><a href="#download-app">Mobile-app</a></li>
-          <li><a href="#footer">Contact Us</a></li>
+
+          <li onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>
+            <Link to="/">Home</Link>
+          </li>
+
+          <li onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>
+            <a href="#explore-menu">Menu</a>
+          </li>
+
+
+          <li onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>
+            <a href="#download-app">Mobile-app</a>
+          </li>
+
+
+          <li onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>
+            <a href="#footer">Contact Us</a>
+          </li>
         </ul>
       </div>
 
       <div className="sign-in">
         <ul>
-          <li><Link to="/search"><img src={assets.search_icon} alt="Search" /></Link></li>
           <li>
-            <Link to="/cart"><img className="basket" src={assets.basket_icon} alt="Cart" /></Link>
-            {token && totalItems > 0 && <div className="dot">{totalItems}</div>}
+            <Link to="/search" >
+              <img src={assets.search_icon} alt="Search" />
+            </Link>
           </li>
+
           <li>
-            {!token ? (
+            <Link to="/cart">
+              <img className="basket" src={assets.basket_icon} alt="User" />
+            </Link>
+            {token && totalItems > 0 ?
+              (
+                <div className="dot">
+                  {totalItems}
+                </div>
+
+              ) :
+              (
+                <div></div>
+              )}
+
+
+
+          </li>
+
+          <li>
+            {!token ?
+
               <button className="button" onClick={() => setShowLogin(true)}>Sign in</button>
-            ) : (
+
+              :
               <div className='profile-dropdown'>
                 <div className='profile-icon'>
-                  {userData?.name ? userData.name[0].toUpperCase() : ""}
+                  {userData && userData?.name[0].toUpperCase()}
                 </div>
                 <ul>
-                  <Link to="/myorders"><li><img src={assets.parcel_icon} alt="" />Orders</li></Link>
-                  <li onClick={handleLogout}><img src={assets.logout_icon} alt="" />Logout</li>
+                  <Link to="/myorders"><li><img src={assets.parcel_icon} />Orders</li></Link>
+                  <li onClick={handleLogout}><img src={assets.logout_icon} />Logout</li>
                 </ul>
               </div>
-            )}
+            }
+
           </li>
         </ul>
       </div>
